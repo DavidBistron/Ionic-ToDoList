b0c00e681b5b1af8103c5df77939a84f7f8c9662
DavidBistron <154263696+DavidBistron@users.noreply.github.com>
2024-11-22 19:53:36 +0100

    Version 1: Create ToDos, mark as done, undone or delete

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,70 @@
 import { Component } from '@angular/core';
-import { addIcons } from 'ionicons';
-import { logoIonic, calendar, add } from 'ionicons/icons';
-import { IonHeader, IonIcon, IonToolbar, IonTitle, IonContent, IonButton, IonCheckbox, IonGrid, IonRow, IonCol, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonCard, IonFab, IonFabButton, IonList, IonListHeader, IonLabel, IonItem } from '@ionic/angular/standalone';
+import { AlertController, IonAlert, IonText, IonHeader, IonInput, IonReorderGroup, IonReorder, IonSegment, IonSegmentButton, IonIcon, IonToolbar, IonTitle, IonContent, IonButton, IonCheckbox, IonGrid, IonRow, IonCol, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonCard, IonFab, IonFabButton, IonList, IonListHeader, IonLabel, IonItem } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 
 // Used components
 @Component({
-  // html, scss names
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [CommonModule, IonHeader, IonIcon, IonToolbar, IonTitle, IonContent, IonButton, IonCheckbox, IonGrid, IonRow, IonCol, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonCard, IonFab, IonFabButton, IonList, IonListHeader, IonLabel, IonItem],
+  imports: [CommonModule, IonAlert, IonText, IonHeader, IonInput, IonReorder, IonReorderGroup, IonSegmentButton, IonSegment, IonIcon, IonToolbar, IonTitle, IonContent, IonButton, IonCheckbox, IonGrid, IonRow, IonCol, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonCard, IonFab, IonFabButton, IonList, IonListHeader, IonLabel, IonItem],
 })
 
 // Main Class for handling functions 
 export class HomePage {
-  todoList: string[] = ['Milch', 'Kuchen', 'Essen'];
   
-  constructor() {
-    addIcons({calendar,add,logoIonic});
+  // Constructor
+  constructor(private alertController: AlertController) {}
+
+  // ToDo List with default values
+  todoList: ToDo[] = [
+    { title: 'Shopping', subtitle: 'Food', content: 'Milk and Eggs', actions: ['Done', 'Undone', 'Delete'], isDone: false },
+    { title: 'Household', subtitle: 'Clean', content: 'Wash the dishes', actions: ['Done', 'Undone', 'Delete'], isDone: false },
+    { title: 'Work', subtitle: 'Call CEO', content: 'Discuss annual report', actions: ['Done', 'Undone', 'Delete'], isDone: false }
+  ];
+
+  // Function for adding new ToDo
+  async addToDo() {
+    
   }
+
+
+  // Funktion zur Handhabung von Aktionen
+  handleAction(action: string, todo: ToDo) {
+    if (action === 'Delete') {
+      this.deleteToDo(todo);
+    } else if (action === 'Done') {
+      this.markAsDone(todo);
+    } else if (action === 'Undone') {
+      this.markAsUndone(todo);
+    }
+  }
+
+  // Function: Delete ToDo 
+  deleteToDo(todoToDelete: ToDo) {
+    const index = this.todoList.indexOf(todoToDelete);
+    if (index > -1) {
+      this.todoList.splice(index, 1); // Delete ToDo from List
+    }
+  }
+
+  // Function: Mark ToDo as done
+  markAsDone(todoToMark: ToDo) {
+    todoToMark.isDone = true;
+    
+  } 
+
+  // Function: Mark ToDo as undone
+  markAsUndone(todoToMark: ToDo) {
+    todoToMark.isDone = false;
+  }
+}
+
+export interface ToDo {
+  title: string;
+  subtitle: string;
+  content: string;
+  actions: string[];
+  isDone: boolean;
 }
